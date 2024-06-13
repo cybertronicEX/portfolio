5547036ad8ee3b51275946127bf6664320ca0688
CybertronEX <your.visaldeminda@gmail.com>
2024-06-13 23:23:41 +0530

    feat: background image

--- src/App.js
+++ src/App.js
@@ -7,21 +7,26 @@ import CareerDevelopmentPlan from './components/CareerDevelopmentPlan';
 import CurriculumVitae from './components/CurriculumVitae';
 import Certificate from './components/Certificate';
 import './App.css';
+import BackgroundSVG from './assets/Asset.svg'; // Import the SVG file
 
 const App = () => {
   return (
     <Router>
       <div className="App">
+      <div className="background-animation">
+          <img src={BackgroundSVG} alt="background" className="svg-background" />
+        </div>
         <Header />
-    
-        <Routes>
-          <Route path="/introduction" element={<Introduction />} />
-          <Route path="/reflective-journal" element={<ReflectiveJournal />} />
-          <Route path="/career-development-plan" element={<CareerDevelopmentPlan />} />
-          <Route path="/curriculum-vitae" element={<CurriculumVitae />} />
-          <Route path="/certificate" element={<Certificate />} />
-          <Route path="/" element={<Introduction />} />
-        </Routes>
+        <div className="content">
+          <Routes>
+            <Route path="/introduction" element={<Introduction />} />
+            <Route path="/reflective-journal" element={<ReflectiveJournal />} />
+            <Route path="/career-development-plan" element={<CareerDevelopmentPlan />} />
+            <Route path="/curriculum-vitae" element={<CurriculumVitae />} />
+            <Route path="/certificate" element={<Certificate />} />
+            <Route path="/" element={<Introduction />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
